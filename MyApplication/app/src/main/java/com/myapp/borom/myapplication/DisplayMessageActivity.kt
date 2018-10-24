24bc101bd55f60bb68ca13784e2a2174cbc9723a
empty-space <zergostanIND31@gmail.com>
2018-10-24 15:15:10 +0300

    Initial commit

---
+++ MyApplication/app/src/main/java/com/myapp/borom/myapplication/DisplayMessageActivity.kt
@@ -0,0 +1,20 @@
+package com.myapp.borom.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_display_message.*
+
+class DisplayMessageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+
+        // Get the Intent that started this activity and extract the string
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        // Capture the layout's TextView and set the string as its text
+        textView1.text = message
+
+    }
+}
---
+++ MyApplication/app/src/main/java/com/myapp/borom/myapplication/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.myapp.borom.myapplication
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val EXTRA_MESSAGE = "com.myapp.extra.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+    val list_hello_kotlin = arrayOf("Hello Kotlin World!","Hello!","Hello Andri!")
+    var hello_index=0
+
+    fun getNextHello():String{
+        hello_index=(hello_index+1) % list_hello_kotlin.size
+        return list_hello_kotlin[hello_index]
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /*btn_go.setOnClickListener {
+            //editTextField.setText(getNextHello())
+        }*/
+    }
+
+    fun go(view: View) {
+        val message = editTextField.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+}
