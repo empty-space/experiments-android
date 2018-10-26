ab5aad6587007c9d2152f7c9f8366b1670025eba
empty-space <zergostanIND31@gmail.com>
2018-10-26 17:41:40 +0300

    Added app2
    Done app2.
    Some improvements to app1

--- MyApplication/app/src/main/java/com/myapp/borom/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/myapp/borom/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.text.method.LinkMovementMethod
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
@@ -24,6 +25,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //make link working
+        link_github.setMovementMethod(LinkMovementMethod.getInstance());
         /*btn_go.setOnClickListener {
             //editTextField.setText(getNextHello())
         }*/
---
+++ MyApplication/app2/src/main/java/com/myapp/borom/app2/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.myapp.borom.app2
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlin.system.exitProcess
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun goToActivity1(view: View) {
+        startActivity(Intent(this, activity1::class.java))
+    }
+    fun goToActivity2(view: View) {
+        startActivity(Intent(this, activity2::class.java))
+    }
+    fun goToActivity3(view: View) {
+        startActivity(Intent(this, activity3::class.java))
+    }
+    fun exit(view: View) {
+        finish()
+    }
+}
