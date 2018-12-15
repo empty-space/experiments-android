cfb339ec9149ac2f0aed4a3bb2e89ad53bebf2c4
empty-space <zergostanIND31@gmail.com>
2018-12-15 08:48:44 +0200

    app7 Initial

---
+++ MyApplication/app7/src/main/java/com/myapp/borom/app7/activity3.kt
@@ -0,0 +1,12 @@
+package com.myapp.borom.app7
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class activity3 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activity3)
+    }
+}
